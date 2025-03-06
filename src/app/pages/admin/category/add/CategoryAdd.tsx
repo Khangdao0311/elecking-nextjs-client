a08eaa0ac6985a1d76371c21d0f9b11e7e96d49a
thientruong <thientruong251192@gmail.com>
2025-03-06 15:30:41 +0700

    cập nhật trang thêm danh mục

--- src/app/pages/admin/category/add/CategoryAdd.tsx
+++ src/app/pages/admin/category/add/CategoryAdd.tsx
@@ -1,5 +1,65 @@
+"use client";
+import Button from "@/app/components/admin/Button";
+import TitleAdmin from "@/app/components/admin/TitleAdmin";
+import { Input, Select } from "antd";
+import React from "react";
+import { IoCloseSharp } from "react-icons/io5";
+
+const handleChange = (value: string) => {
+  console.log(`selected ${value}`);
+};
 function CategoryAdd() {
-  return <div>Trang Thêm Mới Danh Mục</div>;
+  return (
+    <>
+      <TitleAdmin title="Quản lý danh mục / Sửa danh mục" />
+      <div className="bg-white shadow-xl rounded-lg px-4 py-4 flex items-start flex-col gap-4">
+        <div className="w-full flex flex-col gap-6">
+          <div className="w-full">
+            <div className="w-full p-2.5 text-2xl font-semibold border-b-2 border-primary">
+              Tạo Mới Danh Mục
+            </div>
+          </div>
+          <div className="flex items-center gap-4">
+            <div className="flex gap-0.5 flex-col">
+              <div className="text-sm font-medium">Tên Danh Mục <span className="text-primary">*</span></div>
+              <Input
+                className="w-[268px] h-11 shadow-md"
+                placeholder="Nhập Tên Danh Mục"
+              />
+            </div>
+            
+          </div>
+          <div>
+            <div className="text-sm font-medium">
+              Ảnh <span className="text-primary">*</span>
+            </div>
+            <div className="flex gap-2.5 flex-col">
+              <div className="flex items-center gap-2.5 bg-white border border-gray-100 shadow-md p-1.5">
+                <div className="w-[106px] h-auto text-sm font-normal pt-1.5 pb-1.5 pr-5 pl-5 bg-gray-300 border border-gray-100 rounded">
+                  Chọn tệp
+                </div>
+                <div className="w-full text-sm font-normal">
+                  <span>1</span>Tệp
+                </div>
+              </div>
+              <div className="flex items-center flex-wrap gap-3">
+                <div className="w-20 h-20 relative">
+                  <img
+                    src="https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/d/i/dien-thoai-samsung-galaxy-s25-ultra_3__3.png"
+                    alt=""
+                  />
+                  <div className="w-3 h-3 bg-white absolute top-0 right-0 flex items-center justify-center mt-1 mr-1">
+                    <IoCloseSharp />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <Button />
+      </div>
+    </>
+  );
 }
 
 export default CategoryAdd;
