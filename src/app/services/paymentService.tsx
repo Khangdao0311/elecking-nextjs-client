f5a0c9029c6d2ab89d11c25b6144b878172437d4
ĐÀO VĨNH KHANG <khangdao0311@gmail.com>
2025-03-21 14:06:01 +0700

    chỉnh sửa payment service

--- src/app/services/paymentService.tsx
+++ src/app/services/paymentService.tsx
@@ -1,18 +1,11 @@
 import axios from "axios";
 import config from "@/app/config";
 
-export async function getpayment(params: any) {
+export async function getQuery(params: any) {
   return axios
-    .get(`${config.api.payment}`, { params })
+    .get(`${config.api.payment_method}`, { params })
     .then((response: any) => response.data);
 }
 export async function getById(id: string) {
-  return axios
-    .get(`${config.api.payment_method}/${id}`)
-    .then((response: any) => response.data);
-}
-export async function getQuery() {
-  return axios
-    .get(`${config.api.payment_method}`)
-    .then((response: any) => response.data);
+  return axios.get(`${config.api.payment_method}/${id}`).then((response: any) => response.data);
 }
